9ac27d2bdc23db69cbadf64fcc37a8939fd118a5
Sajal Narang <sajalnarang@gmail.com>
2017-07-26 18:55:19 +0530

    CL324 t01p01

---
+++ CL324/t01p01.m
@@ -0,0 +1,17 @@
+clear all;
+clc;
+
+k = [0.00043 0.00103 0.00180 0.00355 0.00717];
+T = [313 319 323 328 333];
+
+n = length(T);
+x = 1./T;
+y = log(k);
+
+[P,S] = polyfit(x,y,1);
+fitConc = polyval(P,x);
+plot(x,y,'o',x,fitConc,'-');
+title('ln(k) vs 1/T');
+xlabel('1/T (K^{-1})');
+ylabel('ln(k)');
+legend('Experimental Data', 'Prediction');
